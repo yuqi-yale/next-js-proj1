7e83c19cbbe1dc9f38c9ef1d74cccdbae2f8af08
yuqi-yale <yuqi.zhao@yale.edu>
2024-09-28 16:28:03 -0400

    image debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import { Icons } from "@/data/data";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const Picture = [
-  {pos: 'https://next-js-proj1.vercel.app/images/bg1.png'},
-  {pos: 'https://next-js-proj1.vercel.app/images/bg2.png'},
-  {pos: 'https://next-js-proj1.vercel.app/images/bg3.png'},
-  {pos: 'https://next-js-proj1.vercel.app/images/bg4.png'},
-  {pos: 'https://next-js-proj1.vercel.app/images/bg5.png'},
+  { pos: 'https://next-js-proj1.vercel.app/images/bg1.png' },
+  { pos: 'https://next-js-proj1.vercel.app/images/bg2.png' },
+  { pos: 'https://next-js-proj1.vercel.app/images/bg3.png' },
+  { pos: 'https://next-js-proj1.vercel.app/images/bg4.png' },
+  { pos: 'https://next-js-proj1.vercel.app/images/bg5.png' },
 ]
 
 function getTodayDateKey() {
@@ -41,19 +42,27 @@ export default function HomePage() {
   }
 
   return (
-    <div
-      className="flex items-end justify-center h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="flex space-x-20 mb-16">
+    <div>
+      <div
+        className="flex items-end justify-center h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="flex space-x-20 mb-16">
 
-        {Icons.map((icon, index) => (
-          <a href={icon.herf} className="flex flex-col items-center"  key={index}>
-          <icon.pos className="w-6 h-6"/>
-        </a>
-        ))}
+          {Icons.map((icon, index) => (
+            <a href={icon.herf} className="flex flex-col items-center" key={index}>
+              <icon.pos className="w-6 h-6" />
+            </a>
+          ))}
 
+        </div>
+        <div>
+          <Image width={500} height={500} src='/images/bg1.png' alt="">
+
+          </Image>
+        </div>
       </div>
+
     </div>
   );
 }
